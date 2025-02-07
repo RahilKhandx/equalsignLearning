71ef8f5447894cb8e201bbdc42beaf12305c8123
Bhumi005jk <bhumi0055jk@gmail.com>
2025-02-07 14:10:04 +0530

    add some equal

--- belief.php
+++ belief.php
@@ -29,84 +29,136 @@
                         <div class="row align-items-center">
                             <div class="col-lg-12  md-pl-15 md-pr-15 md-mb-30">
                                 <div class="img-part">
-                                    <img src="assets/images/about/history.png" alt="About Image">
+                                    <!-- <img src="assets/images/about/history.png" alt="About Image"> -->
                                 </div>
                                 <ul class="nav nav-tabs  histort-part" id="myTab" role="tablist">
                                     <li class="nav-item tab-btns single-history wow fadeInUp" data-wow-delay="100ms"
                                         data-wow-duration="2000ms">
                                         <a class="nav-link tab-btn active" id="about-history-tab" data-toggle="tab"
                                             href="#about-history" role="tab" aria-controls="about-history"
-                                            aria-selected="true"><span class="icon-part"><i class="flaticon-banknote"></i></span>Our <br>
+                                            aria-selected="true"><span class="icon-part"><i
+                                                    class="flaticon-banknote"></i></span>Our <br>
                                             Mission</a>
                                     </li>
                                     <li class="nav-item tab-btns single-history wow fadeInUp" data-wow-delay="100ms"
                                         data-wow-duration="2000ms">
-                                        <a class="nav-link tab-btn" id="about-mission-tab" data-toggle="tab" href="#about-mission"
-                                            role="tab" aria-controls="about-mission" aria-selected="false"><span
-                                                class="icon-part"><i class="flaticon-flower"></i></span>Our <br> Vision</a>
+                                        <a class="nav-link tab-btn" id="about-mission-tab" data-toggle="tab"
+                                            href="#about-mission" role="tab" aria-controls="about-mission"
+                                            aria-selected="false"><span class="icon-part"><i
+                                                    class="flaticon-flower"></i></span>Our <br> Vision</a>
                                     </li>
-                                    <li class="nav-item tab-btns single-history last-item wow fadeInUp" data-wow-delay="100ms"
-                                        data-wow-duration="2000ms">
-                                        <a class="nav-link tab-btn" id="about-admin-tab" data-toggle="tab" href="#about-admin"
-                                            role="tab" aria-controls="about-admin" aria-selected="false"><span class="icon-part"><i
+                                    <li class="nav-item tab-btns single-history last-item wow fadeInUp"
+                                        data-wow-delay="100ms" data-wow-duration="2000ms">
+                                        <a class="nav-link tab-btn" id="about-admin-tab" data-toggle="tab"
+                                            href="#about-admin" role="tab" aria-controls="about-admin"
+                                            aria-selected="false"><span class="icon-part"><i
                                                     class="flaticon-analysis"></i></span>Our <br> Values</a>
                                     </li>
                                 </ul>
                             </div>
                             <div class="col-lg-12">
                                 <div class="tab-content tabs-content" id="myTabContent">
+                                    <!-- mission -->
                                     <div class="tab-pane tab fade show active" id="about-history" role="tabpanel"
                                         aria-labelledby="about-history-tab">
-                                        <div class="sec-title ">
-                                            <h2 class="title">Our Mission</h2>
-                                            <div class="desc">
-                                                <p>Equalsign Learning passionately commits to guiding students on a
-                                                    transformative educational journey. We strive to ignite intellectual curiosity,
-                                                    foster critical thinking, and instill a profound love for learning. Our mission
-                                                    is
-                                                    to provide personalized coaching that empowers students to excel academically,
-                                                    preparing them for success in a dynamic and ever-evolving world.</p>
+                                        <div class="row">
+                                            <div class="col-md-6">
+                                                <div class="sec-title ">
+                                                    <h2 class="title">Our Mission</h2>
+                                                    <div class="desc">
+                                                        <p>Equalsign Learning passionately commits to guiding students
+                                                            on a
+                                                            transformative educational journey. We strive to ignite
+                                                            intellectual curiosity,
+                                                            foster critical thinking, and instill a profound love for
+                                                            learning. Our mission
+                                                            is
+                                                            to provide personalized coaching that empowers students to
+                                                            excel academically,
+                                                            preparing them for success in a dynamic and ever-evolving
+                                                            world.</p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <div class="tab-img">
+                                                    <img class="" src="assets\images\Equalsign Imaes\Our Mission.png"
+                                                        alt="Tab Image">
+                                                </div>
                                             </div>
                                         </div>
-                                        <div class="tab-img">
-                                            <img class="" src="assets\images\Equalsign Imaes\Our Mission.png" alt="Tab Image">
-                                        </div>
+
+
                                     </div>
+                                    <!-- vission -->
                                     <div class="tab-pane fade" id="about-mission" role="tabpanel"
                                         aria-labelledby="about-mission-tab">
-                                        <div class="sec-title ">
-                                            <h2 class="title">Our Vision</h2>
-                                            <div class="desc">
-                                                <p>At Equalsign Learning, we envision a future where each student,
-                                                    equipped with our holistic education, not only achieves academic excellence but
-                                                    also
-                                                    emerges as a confident and compassionate individual. Our vision is to be a
-                                                    trailblazer in innovative teaching, recognized for shaping not just successful
-                                                    students but future leaders who contribute positively to the global community.
-                                                </p>
+                                        <div class="row">
+                                            <div class="col-md-6">
+                                                <div class="sec-title ">
+                                                    <h2 class="title">Our Vision</h2>
+                                                    <div class="desc">
+                                                        <p>At Equalsign Learning, we envision a future where each
+                                                            student,
+                                                            equipped with our holistic education, not only achieves
+                                                            academic excellence but
+                                                            also
+                                                            emerges as a confident and compassionate individual. Our
+                                                            vision is to be a
+                                                            trailblazer in innovative teaching, recognized for shaping
+                                                            not just successful
+                                                            students but future leaders who contribute positively to the
+                                                            global community.
+                                                        </p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <div class="tab-img">
+                                                    <img class="" src="assets\images\Equalsign Imaes\Our Vission.png"
+                                                        alt="Tab Image">
+                                                </div>
                                             </div>
                                         </div>
-                                        <div class="tab-img">
-                                            <img class="" src="assets\images\Equalsign Imaes\Our Vission.png" alt="Tab Image">
-                                        </div>
+
+
                                     </div>
-                                    <div class="tab-pane fade" id="about-admin" role="tabpanel" aria-labelledby="about-admin-tab">
-                                        <div class="sec-title ">
-                                            <h2 class="title">Our Values</h2>
-                                            <div class="desc">
-                                                <p>At Equalsign Learning, our values form the bedrock of our educational
-                                                    philosophy. We are driven by a commitment to excellence, fostering an
-                                                    environment
-                                                    where intellectual curiosity, innovation, and personalized growth thrive.
-                                                    Integrity,
-                                                    compassion, and a dedication to lifelong learning are the cornerstones of our
-                                                    approach. We believe in empowering students as responsible, compassionate
-                                                    individuals who contribute meaningfully to the global community.</p>
+
+                                    <!-- values -->
+                                    <div class="tab-pane fade" id="about-admin" role="tabpanel"
+                                        aria-labelledby="about-admin-tab">
+                                        <div class="row">
+                                            <div class="col-md-6">
+                                                <div class="sec-title ">
+
+                                                    <h2 class="title">Our Values</h2>
+                                                    <div class="desc">
+                                                        <p>At Equalsign Learning, our values form the bedrock of our
+                                                            educational
+                                                            philosophy. We are driven by a commitment to excellence,
+                                                            fostering an
+                                                            environment
+                                                            where intellectual curiosity, innovation, and personalized
+                                                            growth thrive.
+                                                            Integrity,
+                                                            compassion, and a dedication to lifelong learning are the
+                                                            cornerstones of our
+                                                            approach. We believe in empowering students as responsible,
+                                                            compassionate
+                                                            individuals who contribute meaningfully to the global
+                                                            community.</p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <div class="tab-img">
+                                                    <img class="" src="assets\images\Equalsign Imaes\Our Values.png"
+                                                        alt="Tab Image">
+                                                </div>
                                             </div>
                                         </div>
-                                        <div class="tab-img">
-                                            <img class="" src="assets\images\Equalsign Imaes\Our Values.png" alt="Tab Image">
-                                        </div>
+
+
                                     </div>
                                 </div>
                             </div>
@@ -141,7 +193,8 @@
 
 
                                 <!-- Submit button -->
-                                <button data-mdb-ripple-init type="button" class="btn btn-primary btn-block mb-4 readon2">Send</button>
+                                <button data-mdb-ripple-init type="button"
+                                    class="btn btn-primary btn-block mb-4 readon2">Send</button>
                             </form>
                         </div>
                     </div>
